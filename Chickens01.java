6037be57a45c5c1bc0e2b80c8a2ab0be9805d549
Damian Czernikowski <dczernikowski1@ucvts.tec.nj.us>
2020-02-20 12:15:55 -0500

    second scenario

--- Chickens01.java
+++ Chickens01.java
@@ -4,15 +4,15 @@ package chickens01;
 public class Chickens01 {
     public static void main(String[] args) {
         //Put yout code here
-        int chickenCount = 3;
-        int eggsPerChicken = 5;
+        int chickenCount = 8;
+        int eggsPerChicken = 4;
         int totalEggs = 0;
         totalEggs = totalEggs+(chickenCount*eggsPerChicken);
         System.out.println("Monday:" + totalEggs + " eggs");
-        chickenCount = 4;
+        chickenCount = 9;
         totalEggs = totalEggs+(chickenCount*eggsPerChicken);
         System.out.println("Tuesday:" + totalEggs + " eggs");
-        chickenCount = 2;
+        chickenCount = 4;
         totalEggs = totalEggs+(chickenCount*eggsPerChicken);
         System.out.println("Wednesday:" + totalEggs + " eggs");
         System.out.println("The Farmer collected " + totalEggs + " eggs from Monday to Wednesday.");
